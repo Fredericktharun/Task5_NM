fe58a31e4805a62e59df9cd0c235d579dd5d7ef6
Fredericktharun <91191071+Fredericktharun@users.noreply.github.com>
2023-05-13 21:24:38 +0530

    README.md
    This project is a mini version of a working blogging website. This project is done by using (MongoDB, Express.js, React.js, Node.js).
    
    #Features of this site:
    
    1.Dynamic Blog pages.
    
    2.Have a dedicated editor for blogs.
    
    3.You can add/make as many blogs you want.
    
    4.You can add Headings, paragraphs, and Images to the blog post.
    
    5.Have read more blogs section also.
    
    #Installation Guide.
    
    Once you forked this repo. Make sure to run npm install command inside the folder to install all the package.
    
    #Technologies Used
    
    1.Front-end: React.js, Redux (for state management), HTML5, CSS3
    
    2.Back-end: Node.js, Express.js
    
    3.Database: MongoDB

--- server.js
+++ server.js
@@ -1,23 +1,49 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const Article = require('./models/article')
-const articleRouter = require('./routes/articles')
-const methodOverride = require('method-override')
-const app = express()
+const express = require('express');
+const path = require('path');
+const fileupload = require('express-fileupload');
 
-mongoose.connect('mongodb://localhost/blog', {
-  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+let initial_path = path.join(__dirname, "public");
+
+const app = express();
+app.use(express.static(initial_path));
+app.use(fileupload());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(initial_path, "home.html"));
 })
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-app.use(methodOverride('_method'))
-
-app.get('/', async (req, res) => {
-  const articles = await Article.find().sort({ createdAt: 'desc' })
-  res.render('articles/index', { articles: articles })
+app.get('/editor', (req, res) => {
+    res.sendFile(path.join(initial_path, "editor.html"));
 })
 
-app.use('/articles', articleRouter)
+// upload link
+app.post('/upload', (req, res) => {
+    let file = req.files.image;
+    let date = new Date();
+    // image name
+    let imagename = date.getDate() + date.getTime() + file.name;
+    // image upload path
+    let path = 'public/uploads/' + imagename;
 
-app.listen(5000)
+    // create upload
+    file.mv(path, (err, result) => {
+        if(err){
+            throw err;
+        } else{
+            // our image upload path
+            res.json(`uploads/${imagename}`)
+        }
+    })
+})
+
+app.get("/:blog", (req, res) => {
+    res.sendFile(path.join(initial_path, "blog.html"));
+})
+
+app.use((req, res) => {
+    res.json("404");
+})
+
+app.listen("3000", () => {
+    console.log('listening......');
+})
